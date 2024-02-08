465250690d051de1c53c850467adaf3d3e0d8d9c
maria-ssantos <meikrown@proton.me>
2024-02-08 16:19:02 -0300

    Adicionando meu código ao repositório

---
+++ desafiodio.js
@@ -0,0 +1,26 @@
+let nomeDoHeroi = "";
+let nivelDoHeroi;
+let xpDoHeroi;
+
+if (xpDoHeroi <= 1000) {
+  nivelDoHeroi = "Ferro";
+} else if (xpDoHeroi >= 1001 && xpDoHeroi <= 2000) {
+  nivelDoHeroi = "Bronze";
+} else if (xpDoHeroi >= 2001 && xpDoHeroi <= 5000) {
+  nivelDoHeroi = "Prata";
+} else if (xpDoHeroi >= 5001 && xpDoHeroi <= 7000) {
+  nivelDoHeroi = "Ouro";
+} else if (xpDoHeroi >= 7001 && xpDoHeroi <= 8000) {
+  nivelDoHeroi = "Platina";
+} else if (xpDoHeroi >= 8001 && xpDoHeroi <= 9000) {
+  nivelDoHeroi = "Ascendente";
+} else if (xpDoHeroi >= 9001 && xpDoHeroi <= 10000) {
+  nivelDoHeroi = "Imortal";
+} else {
+  nivelDoHeroi = "Radiante";
+}
+
+//saida
+console.log(
+  "O Herói de nome " + nomeDoHeroi + " está no nível de " + nivelDoHeroi
+);
